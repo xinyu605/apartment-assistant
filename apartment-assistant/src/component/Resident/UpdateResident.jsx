d630402b3fa38a561f5e1a41fb368f7716c927aa
XinyuYan <xinyuyan605@gmail.com>
2020-12-05 16:30:20 +0800

    packing resident input values

--- apartment-assistant/src/component/Resident/UpdateResident.jsx
+++ apartment-assistant/src/component/Resident/UpdateResident.jsx
@@ -10,6 +10,13 @@ export default function UpdateResident() {
   const [floor, setFloor] = useState(0);
   const [address, setAddress] = useState("");
   const [remark, setRemark] = useState("無");
+  const [memberNameList, setMemberNameList] = useState([]);
+  const [memberPhoneList, setMemberPhoneList] = useState([]);
+  const [memberEmailList, setMemberEmailList] = useState([]);
+
+  /**************************************************
+  Create / Delete one more family member input form
+  ***************************************************/
 
   function createInputs(e) {
     e.preventDefault();
@@ -26,37 +33,41 @@ export default function UpdateResident() {
     setFamilyMemberForm(familyMembersForm); //bug: 無法順利更新 familyMemberForm state
   }
 
-  const FamilyMemberForm = familyMembersForm.map((item) => {
-    return (
-      <FamilyMembers
-        id={`familyMembers${item.id}`}
-        key={`familyMembers${item.id}`}
-      />
-    );
-  });
-
   /************************
- Get user's input value
- ***********************/
-  function updateInfo(e) {
-    let inputValue = e.currentTarget.value;
-    switch (e.currentTarget.id) {
-      case "residentNumbers":
-        setResidentNumbers(inputValue);
-        break;
-      case "floor":
-        setFloor(parseInt(inputValue));
-        break;
-      case "address":
-        setAddress(inputValue);
-        break;
-      case "remark":
-        setRemark(inputValue);
-        break;
-      default:
-        break;
+  Packing user's input value
+  **************************/
+
+  function packingInfo(e) {
+    e.preventDefault();
+    let residentNumbers = document.querySelector("#residentNumbers").value;
+    let floor = parseInt(document.querySelector("#floor").value);
+    let address = document.querySelector("#address").value;
+    let remark = document.querySelector("#remark").value;
+    let nameList = [];
+    let phoneList = [];
+    let emailList = [];
+    for (let i = 0; i < familyMembersForm.length; i++) {
+      nameList[i] = document.querySelector(`#inputName${i}`).value;
+      phoneList[i] = document.querySelector(`#inputPhone${i}`).value;
+      emailList[i] = document.querySelector(`#inputEmail${i}`).value;
     }
+
+    setResidentNumbers(residentNumbers);
+    setFloor(floor);
+    setAddress(address);
+    setRemark(remark);
+    setMemberNameList(nameList);
+    setMemberPhoneList(phoneList);
+    setMemberEmailList(emailList);
   }
+
+  /****************************
+   FamilyMembersForm component
+   ****************************/
+
+  const FamilyMemberForm = familyMembersForm.map((item) => {
+    return <FamilyMembers id={`family${item.id}`} key={`family${item.id}`} />;
+  });
 
   return (
     <div className={styles.updateResident}>
@@ -65,56 +76,38 @@ export default function UpdateResident() {
           id="residentNumbers"
           type="text"
           placeholder="請填寫戶號"
-          onChange={updateInfo}
         ></input>
-        <input
-          id="floor"
-          placeholder="請填寫樓層"
-          onChange={updateInfo}
-        ></input>
-        <input
-          id="address"
-          placeholder="請填寫地址"
-          onChange={updateInfo}
-        ></input>
-        <input
-          id="remark"
-          type="text"
-          placeholder="請填寫備註"
-          onChange={updateInfo}
-        ></input>
+        <input id="floor" placeholder="請填寫樓層"></input>
+        <input id="address" placeholder="請填寫地址"></input>
+        <input id="remark" type="text" placeholder="請填寫備註"></input>
         <div className={styles.familyMemberList}>
           <h3>住戶成員</h3>
           {FamilyMemberForm}
           <button onClick={createInputs}>+</button>
           <button onClick={deleteLastInput}>-</button>
         </div>
+        <button onClick={packingInfo}>確定</button>
       </form>
     </div>
   );
 }
 
 function FamilyMembers(props) {
-  console.log(props);
+  const number = props.id.slice(12);
   return (
-    <div
-      className={styles.familyMember}
-      id={`familymember${props.id}`}
-      key={`familymember${props.id}`}
-    >
+    <div className={styles.familyMember} id={`${props.id}`} key={`${props.id}`}>
       <input
-        id={`inputName${props.id}`}
+        id={`inputName${number}`}
         type="text"
         placeholder="請填寫姓名"
-        // onChange={updateMemberName}
       ></input>
       <input
-        id={`inputPhone${props.id}`}
+        id={`inputPhone${number}`}
         type="text"
         placeholder="請填寫聯絡電話"
       ></input>
       <input
-        id={`inputEmail${props.id}`}
+        id={`inputEmail${number}`}
         type="text"
         placeholder="請填寫Email"
       ></input>
