657d8bda99823345c3408cf39c5e9e1483d3f34b
XinyuYan <xinyuyan605@gmail.com>
2020-12-30 17:19:40 +0800

    refactor EntryBoard component v.1

--- apartment-assistant/src/component/Entry/EntryBoard.jsx
+++ apartment-assistant/src/component/Entry/EntryBoard.jsx
@@ -15,47 +15,6 @@ export default function EntryBoard() {
     }
   }, []);
 
-  const IssueList = issues.map((issue) => {
-    const updateTime = showDate(issue.updateTime.seconds);
-    const deadline = showDate(issue.deadline.seconds);
-    const index = issues.indexOf(issue);
-
-    function toggleDetail(e) {
-      const detail = document.querySelector(`#detail${index}`);
-      console.log(e.currentTarget.value);
-      if (detail.style.display === "none") {
-        detail.style.display = "grid";
-        e.currentTarget.textContent = "關閉";
-      } else {
-        detail.style.display = "none";
-        e.currentTarget.textContent = "閱讀";
-      }
-    }
-
-    return (
-      <li className={styles.issue} key={nanoid()}>
-        <p className={styles.updateTime}>{updateTime}</p>
-        <h3 className={styles.issueTopic}>{issue.topic}</h3>
-        <button
-          id={`readBtn${index}`}
-          className={styles.readMoreBtn}
-          onClick={toggleDetail}
-        >
-          閱讀
-        </button>
-        <div
-          id={`detail${index}`}
-          className={styles.issueDetail}
-          style={{ display: "none" }}
-        >
-          <p className={styles.author}>發布者：{issue.author}</p>
-          <p className={styles.deadline}>公告期限：{deadline}</p>
-          <p className={styles.content}>{issue.content}</p>
-        </div>
-      </li>
-    );
-  });
-
   return (
     <div className={styles.issues} id="issues">
       <div className={styles.title}>
@@ -65,7 +24,40 @@ export default function EntryBoard() {
         <h2>社區公告</h2>
       </div>
 
-      <ul className={styles.issueList}>{IssueList}</ul>
+      <ul className={styles.issueList}>
+        {issues.map((issue) => {
+          return <Issue issue={issue} />;
+        })}
+      </ul>
     </div>
+  );
+}
+
+function Issue(props) {
+  const [isShowing, setShowing] = useState(false);
+  const updateTime = showDate(props.issue.updateTime.seconds);
+  const deadline = showDate(props.issue.deadline.seconds);
+
+  function toggleDetail(e) {
+    e.preventDefault();
+    isShowing ? setShowing(false) : setShowing(true);
+  }
+
+  return (
+    <li className={styles.issue} key={nanoid()}>
+      <p className={styles.updateTime}>{updateTime}</p>
+      <h3 className={styles.issueTopic}>{props.issue.topic}</h3>
+      <button className={styles.readMoreBtn} onClick={toggleDetail}>
+        {isShowing ? "關閉" : "閱讀"}
+      </button>
+      <div
+        className={styles.issueDetail}
+        style={isShowing ? { display: "grid" } : { display: "none" }}
+      >
+        <p className={styles.author}>發布者：{props.issue.author}</p>
+        <p className={styles.deadline}>公告期限：{deadline}</p>
+        <p className={styles.content}>{props.issue.content}</p>
+      </div>
+    </li>
   );
 }
